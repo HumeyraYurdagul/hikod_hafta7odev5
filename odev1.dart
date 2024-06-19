ba153d5258c68a68f5ac3e41bc8599d13f22dce9
HumeyraYurdagul <96681753+HumeyraYurdagul@users.noreply.github.com>
2024-06-19 16:31:43 +0300

    Add files via upload

---
+++ odev1.dart
@@ -0,0 +1,53 @@
+void main() {
+
+  // A
+
+  // int
+  int sayi = 2;
+  print("int değişkeni: $sayi");
+
+  //string
+  String yazi = "Hello World";
+  print("string değişkeni: $yazi");
+
+  //double
+  double OndalikliSayi = 3.5;
+  print("double değişkeni: $OndalikliSayi");
+
+  //bool
+  bool online_mi = true;
+  print("bool değişkeni: $online_mi");
+
+  //dynamic
+  dynamic dinamikDeger = "metin";
+  print("dynamic değişkeni: $dinamikDeger");
+
+  //char
+  String metin = "Merhaba";
+  print("char değişkeni: $metin");
+
+  // B
+
+  // camelCase
+  var ondalikliSayi = 3.5;
+  print("double değişkeni (camelCase): $ondalikliSayi");
+
+  //snake_case
+  var dinamik_deger = "metin";
+  print("dynamic değişkeni (snake_case): $dinamik_deger");
+
+  //PascalCase
+  var onlineMi = true;
+  print("bool değişkeni (PascalCase): $onlineMi");
+
+  // C
+
+  String ad = "Hümeyra";
+  String soyad = "Yurdagül";
+  int yas = 22;
+  bool resitlik = true;
+
+  print("Merhaba, ben $ad $soyad. $yas yaşındayım. Reşitlik durumum: $resitlik.");
+
+
+}
